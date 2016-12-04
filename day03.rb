5753263bbd6a88e91f5a49709ed493e3d6a412e1
Luis F Gutierrez <luis@gutierrezhiller.com>
2016-12-04 02:13:25 -0800

    Day 3, puzzle 2 solution

--- day03.rb
+++ day03.rb
@@ -3,7 +3,19 @@ def valid_triangle?(sides)
   (sorted_sides[0] + sorted_sides[1]) > sorted_sides[2]
 end
 
-puts File.readlines('day03_input.txt')
-         .map { |line| line.split.map(&:to_i) }
-         .select { |sides| valid_triangle?(sides) }
-         .count
+def read_input_numbers
+ File.readlines('day03_input.txt')
+     .map { |line| line.split.map(&:to_i) }
+end
+
+def count_triangles(array_of_array_of_sides)
+  array_of_array_of_sides
+      .select { |sides| valid_triangle?(sides) }
+      .count
+end
+
+puts read_input_numbers
+         .transpose
+         .map { |old_column| count_triangles(old_column.each_slice(3).to_a) }
+         .inject(&:+)
+
