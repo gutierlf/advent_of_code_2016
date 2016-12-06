97b9dc300e17441fa0fa6e8d51cd6c43025eb712
Luis F Gutierrez <luis@gutierrezhiller.com>
2016-12-06 00:55:31 -0800

    Day 6, puzzle 1 solution

---
+++ day06.rb
@@ -0,0 +1,15 @@
+def sort_frequencies_hash(hash)
+  hash.sort_by { |_key, value| value }
+end
+
+def compute_frequencies_hash(str)
+  str.scan(/\w/).inject(Hash.new(0)) do |h, c|
+    h[c] += 1
+    h
+  end
+end
+
+message = File.readlines('day06_input.txt').map { |line| line.chars[0..-2] }.transpose
+              .map { |chars| sort_frequencies_hash(compute_frequencies_hash(chars.join))[-1][0] }
+
+puts message.join
