a591d853bb454debc1bb4d6d879280c637be5dc0
Luis F Gutierrez <luis@gutierrezhiller.com>
2016-12-06 00:35:31 -0800

    Day 5, puzzle 2 solution

--- day05.rb
+++ day05.rb
@@ -3,18 +3,26 @@ require 'digest'
 EXAMPLE_DOOR_ID = 'abc'
 ACTUAL_DOOR_ID = 'abbhdwsy'
 
+def interesting?(hash)
+  hash.start_with?('00000') && hash[5] =~ /[0-7]/
+end
+
 def add_next_character(state)
+  password = state[:password]
   i = state[:i]
   loop do
     hash = Digest::MD5.hexdigest(ACTUAL_DOOR_ID + i.to_s)
     i += 1
-    next_char = hash.start_with?('00000') ? hash[5] : nil
-    break {password: state[:password] + next_char, i: i} if next_char
+    index = interesting?(hash) ? hash[5].to_i : nil
+    if index && password[index].empty?
+      password[index] = hash[6]
+      break {password: password, i: i}
+    end
   end
 end
 
-state = 8.times.inject(password: '', i: 0) do |state|
+state = 8.times.inject(password: ([''] * 8), i: 0) do |state|
   add_next_character(state)
 end
 
-puts state[:password]
+puts state[:password].join
